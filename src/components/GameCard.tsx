3a1d6aa4caaa8712b5619e6265143bd61a11a73a
Normanpozuelos <112310927+Normanpozuelos@users.noreply.github.com>
2024-04-14 12:55:44 +0200

    Refactor- move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformiconList from "./PlatformiconList";
 import CriticScore from "./CriticScore";
